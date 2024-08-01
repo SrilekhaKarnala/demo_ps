488651c64cd3db26323b7e5883f26d891dd392ef
srilekha <srilekha.karnala@zemosolabs.com>
2024-08-01 18:06:49 +0530

    Implement Binary Search Algorithm

---
+++ BinarySearch.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class BinarySearch {
+    
+    public static int binarySearch(int[] arr, int key) {
+        int low = 0;
+        int high = arr.length - 1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (arr[mid] == key) {
+                return mid;
+            }
+            else if (arr[mid] < key) {
+                low = mid + 1;
+            }
+            else {
+                high = mid - 1;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of elements in the array: ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        System.out.println("Enter the elements of the array in sorted order: ");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.println("Enter Key: ");
+        int key=sc.nextInt();
+        int result = binarySearch(arr, key);
+
+        if (result == -1) {
+            System.out.println("Element not present");
+        } else {
+            System.out.println("Element found at index " + result);
+        }
+    }
+}
